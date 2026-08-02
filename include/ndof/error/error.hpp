02787720cbeee362ae57816212918d48cebe9354
bob-kerner <rekerner@gmail.com>
2026-08-02 08:17:09 +0000

    Added ugly code to kill stupid tidy-clang admonishment.

--- include/ndof/error/error.hpp
+++ include/ndof/error/error.hpp
@@ -5,6 +5,7 @@
 #include <typeindex>
 #include <type_traits>
 #include <optional>
+#include <string_view>
 #include <stdexcept>
 #include <utility>
 
@@ -137,6 +138,15 @@ namespace ndof::error {
 
         [[nodiscard]] const char* what() const noexcept override;
 
+    protected:
+        template<typename OtherAllocator>
+        ConditionCheckException(
+            const std::basic_string<char, std::char_traits<char>, OtherAllocator>& expression,
+            const std::basic_string<char, std::char_traits<char>, OtherAllocator>& message,
+            const std::source_location& location,
+            CheckMode check_mode
+        );
+
     private:
         allocated_string expression;
         allocated_string message;
@@ -235,14 +245,127 @@ namespace ndof::error {
 
     template<typename ExceptionType, typename Allocator = std::allocator<char>>
     struct ExplicitInnerException : InnerException<Allocator> {
-        explicit ExplicitInnerException(ExceptionType& exception)
-            : InnerException<Allocator>(exception) {
+
+        // Note: This was added to dismiss a clang-tidy warning about not properly moving
+        //       exception before forwarding it to the base class constructor.
+        template<typename ForwardedException>
+        requires std::is_same_v<std::remove_cvref_t<ForwardedException>, ExceptionType>
+        explicit ExplicitInnerException(ForwardedException&& exception)
+            : InnerException<Allocator>(std::forward<ForwardedException>(exception)) {
         }
 
     };
 
     template<typename ExceptionType, typename Allocator = std::allocator<char>>
     void throw_ndof_exception(ExceptionType&& exception) {
-        throw ExplicitInnerException<ExceptionType, Allocator>(std::forward<ExceptionType>(exception));
+        using captured_exception_type = std::remove_cvref_t<ExceptionType>;
+        throw ExplicitInnerException<captured_exception_type, Allocator>(std::forward<ExceptionType>(exception));
+    }
+
+    namespace detail {
+
+        [[nodiscard]] inline std::string_view build_mode_to_string(const BuildMode mode) noexcept {
+            switch (mode) {
+            case BuildMode::debug:
+                return "debug";
+            case BuildMode::release:
+                return "release";
+            case BuildMode::undefined:
+            default:
+                return "undefined";
+            }
+        }
+
+    } // namespace detail
+
+    template<typename Allocator>
+    auto Exception<Allocator>::to_string() const -> allocated_string {
+        allocated_string output;
+        output.reserve(96);
+        output += "Exception{";
+        output += "file=";
+        output += get_file_name();
+        output += ", function=";
+        output += get_function_name();
+        output += ", line=";
+        const auto line_text = std::to_string(get_line());
+        output += line_text.c_str();
+        output += ", mode=";
+        output += detail::build_mode_to_string(get_build_mode()).data();
+        output += "}";
+        return output;
+    }
+
+    template<typename Allocator>
+    const char* Exception<Allocator>::what() const noexcept {
+        message = to_string();
+        return message.c_str();
+    }
+
+    template<typename Allocator>
+    template<typename OtherAllocator>
+    ConditionCheckException<Allocator>::ConditionCheckException(
+        const std::basic_string<char, std::char_traits<char>, OtherAllocator>& expression_value,
+        const std::basic_string<char, std::char_traits<char>, OtherAllocator>& message_value,
+        const std::source_location& location,
+        CheckMode check_mode_value)
+        : Exception<Allocator>(
+              FileName<Allocator>{allocated_string(location.file_name())},
+              FunctionName<Allocator>{allocated_string(location.function_name())},
+              static_cast<std::uint_least32_t>(location.line()),
+              static_cast<BuildMode>(check_mode_value)),
+          expression(expression_value.c_str()),
+          message(message_value.c_str()),
+          check_mode(check_mode_value) {
+    }
+
+    template<typename Allocator>
+    const char* ConditionCheckException<Allocator>::what() const noexcept {
+        return Exception<Allocator>::what();
+    }
+
+    template<typename Allocator>
+    template<typename OtherAllocator>
+    PreConditionCheckException<Allocator>::PreConditionCheckException(
+        const std::basic_string<char, std::char_traits<char>, OtherAllocator>& expression,
+        const std::basic_string<char, std::char_traits<char>, OtherAllocator>& message,
+        const std::source_location& location,
+        CheckMode check_mode)
+        : ConditionCheckException<Allocator>(expression, message, location, check_mode) {
+    }
+
+    template<typename Allocator>
+    const char* PreConditionCheckException<Allocator>::what() const noexcept {
+        return ConditionCheckException<Allocator>::what();
+    }
+
+    template<typename Allocator>
+    template<typename OtherAllocator>
+    PostConditionCheckException<Allocator>::PostConditionCheckException(
+        const std::basic_string<char, std::char_traits<char>, OtherAllocator>& expression,
+        const std::basic_string<char, std::char_traits<char>, OtherAllocator>& message,
+        const std::source_location& location,
+        CheckMode check_mode)
+        : ConditionCheckException<Allocator>(expression, message, location, check_mode) {
+    }
+
+    template<typename Allocator>
+    template<typename OtherAllocator>
+    InvariantConditionCheckException<Allocator>::InvariantConditionCheckException(
+        const std::basic_string<char, std::char_traits<char>, OtherAllocator>& expression,
+        const std::basic_string<char, std::char_traits<char>, OtherAllocator>& message,
+        const std::source_location& location,
+        CheckMode check_mode)
+        : ConditionCheckException<Allocator>(expression, message, location, check_mode) {
+    }
+
+    template<typename Allocator>
+    const char* InvariantConditionCheckException<Allocator>::what() const noexcept {
+        return ConditionCheckException<Allocator>::what();
+    }
+
+    template<typename Allocator>
+    const char* InnerException<Allocator>::what() const noexcept {
+        return Exception<Allocator>::what();
     }
 }
