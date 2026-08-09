261a153ce0d8459634cf64dd2b6c675c17e59f3a
bob-kerner <rekerner@gmail.com>
2026-08-09 09:49:18 +0000

    correction: changed the basic_object_stream to an iostream (from an ostream).

--- include/ndof/error/object_stream.hpp
+++ include/ndof/error/object_stream.hpp
@@ -2,25 +2,32 @@
 #define NDOF_OS_OBJECT_STREAM_HPP
 
 #include "object.hpp"
-#include <ostream>
+#include <cstddef>
+#include <istream>
 
 namespace ndof { 
 
-    template<typename CharT = char, typename Allocator = std::allocator<CharT>>
-    struct basic_object_ostream :
-        public std::basic_ostream<CharT, std::char_traits<CharT>> {
+    template<typename CharT = char, typename Allocator = std::allocator<std::byte>>
+    struct basic_object_iostream :
+        public std::basic_iostream<CharT, std::char_traits<CharT>> {
     public:
         using allocator_type = Allocator;
-        using allocated_string = std::basic_string<CharT, std::char_traits<CharT>, allocator_type>;
+        using object_type = basic_object<CharT, allocator_type>;
+        using char_allocator = typename std::allocator_traits<allocator_type>::template rebind_alloc<CharT>;
+        using allocated_string = std::basic_string<CharT, std::char_traits<CharT>, char_allocator>;
 
-        basic_object_ostream(const basic_object<CharT, allocator_type>& object, const allocator_type& allocator = allocator_type())
-            : allocator_(allocator), object_(object) {
+        explicit basic_object_iostream(const object_type& object, const allocator_type& allocator = allocator_type())
+            : std::basic_iostream<CharT, std::char_traits<CharT>>(nullptr),
+              allocator_(allocator),
+              object_(object) {
         }
 
     private:
         allocator_type allocator_;
-        basic_object<CharT, allocator_type> object_;
+        object_type object_;
     };
+
+    using object_iostream = basic_object_iostream<>;
     
 
 }
