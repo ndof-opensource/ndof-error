7c2c42d53158687bad820c5041a80bf664fb8c70
bob-kerner <rekerner@gmail.com>
2026-08-22 06:58:09 +0000

    added note for to_object.  there might be an issue with the allocator in basic_object here.

--- include/ndof/error/error.hpp
+++ include/ndof/error/error.hpp
@@ -175,6 +175,9 @@ struct basic_explicit_inner_exception : basic_inner_exception<CharT, Traits, All
                                             const std::source_location& source_location_value,
                                             const OtherAllocator& allocator = OtherAllocator());
 
+          
+    // TODO: figure out how to pass an object containing a foreign allocator.  
+    //       maybe use a requires clause?
     [[nodiscard]] result_t<void, CharT, Traits>
     to_object(ndof::basic_object<CharT, Traits>& obj) const override;
 };
